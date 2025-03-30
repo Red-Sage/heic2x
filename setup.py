c20df25ed71e87bf9f7d24ffd2e24e6b7752bf95
Red-Sage <red.sage@outlook.com>
2025-03-30 13:47:15 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='heic2x',
+    version='0.0.1',
+    description='A quick program to convert from heic to other image formtas.',
+    url='https://github.com/Red-Sage/heic2x.git',
+    author='Red Sage',
+    author_email='red.sage@outlool.com',
+    license='All Rights Reserved',
+    packages=['heic2x'],
+
+)
