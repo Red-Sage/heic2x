97bff8075823aadadd6aca433e6b81916c1a6270
Red-Sage <red.sage@outlook.com>
2025-04-04 12:41:56 -0600

    added requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
+from pathlib import Path
+
+folder = Path(__file__)
+requirements_file = folder.parents[0] / 'requirements.txt'
+
+requires = []
+if requirements_file.exists():
+    with open(requirements_file) as f:
+        requires = f.read().splitlines()
 
 setup(
     name='heic2x',
@@ -8,6 +17,7 @@ setup(
     author='Red Sage',
     author_email='red.sage@outlool.com',
     license='All Rights Reserved',
+    install_requires=requires,
     
 
 
